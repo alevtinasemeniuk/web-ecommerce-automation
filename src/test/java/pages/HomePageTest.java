f25f56ff9800b9c5c2fe0dc74ec9a090f204fca3
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-03-13 16:45:44 -0700

    changed the name of HomePage to HomePageTest

--- src/test/java/pages/HomePageTest.java
+++ src/test/java/pages/HomePageTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HomePage {
+public class HomePageTest {
 
 @Test
     public void testProductsMenu(){
