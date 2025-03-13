048eb77d00403f3419daea976109432c7a2c0c55
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-03-13 16:43:17 -0700

    added HomePage class and first test

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePage {
+
+@Test
+    public void testProductsMenu(){
+
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://automationexercise.com/");
+
+    WebElement productsButton = driver.findElement(By.xpath("//a[@href='/products']"));
+    productsButton.click();
+
+    WebElement textTitle = driver.findElement(By.xpath("//div[@class='features_items']/h2"));
+    String text = textTitle.getText().toLowerCase();
+
+    Assert.assertEquals(text, "all products");
+
+    driver.quit();
+    }
+
+}
