1dfe8cf3ed1fa489a3465abfae012d5481e5a989
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-03-15 20:10:49 -0700

    refactor test

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -8,7 +8,7 @@ import runner.BaseTest;
 public class HomePageTest extends BaseTest {
 
     @Test
-    public void testClickProducts(){
+    public void testClickProducts111(){
 
         String text = new HomePage(getDriver())
                 .clickProducts()
