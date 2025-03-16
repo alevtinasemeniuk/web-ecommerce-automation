c4108547808da7938554c5dee60c9c4177dd770a
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-03-15 21:51:15 -0700

    refactor test

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -2,6 +2,7 @@ package runner;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -19,13 +20,24 @@ public class BaseTest {
         // Использование WebDriverManager для автоматической загрузки драйвера
         WebDriverManager.chromedriver().setup();
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // Проверяем, если мы находимся в CI/CD (например, на GitHub Actions)
+        String ciEnv = System.getenv("CI");
+        if (ciEnv != null && ciEnv.equals("true")) {
+            // В CI включаем headless режим
+            options.addArguments("--headless", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920x1080");
+        } else {
+            // На локальной машине используем обычный браузер
+            // options.addArguments("--headless"); // Оставляем без этой опции для локальной работы
+        }
+
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         driver.get("https://automationexercise.com/");
-
     }
 
     @AfterMethod
