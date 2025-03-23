dcbad6b5cb59e4acc905f24bd5b17b68691e698d
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-03-22 17:49:31 -0700

    refactor tests

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -13,27 +13,26 @@ import java.time.Duration;
 
 public class BaseTest {
     private WebDriver driver;
-    private WebDriverWait wait5;
 
     @BeforeMethod
     public void setUp() {
-        // Использование WebDriverManager для автоматической загрузки драйвера
         WebDriverManager.chromedriver().setup();
-
-        ChromeOptions options = new ChromeOptions();
-
-        String ciRun = System.getenv("CI_RUN");
-        String chromeOptions = System.getenv("CHROME_OPTIONS");
-
-        if (ciRun != null && ciRun.equals("true") && chromeOptions != null) {
-            options.addArguments(chromeOptions.split(" "));
-        } else {
-            options.addArguments("--window-size=1920x1080");
-        }
-
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(buildChromeOptions());
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://automationexercise.com/");
+    }
+
+    private ChromeOptions buildChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        String ci = System.getenv("CI");
+        String chromeOpts = System.getenv("CHROME_OPTIONS");
+        if (ci.equals("true") && chromeOpts != null) {
+            options.addArguments(chromeOpts.split("\\s+"));
+        }
+        else {
+            options.addArguments("--window-size=1920x1080");
+        }
+        return options;
     }
 
     @AfterMethod
@@ -47,10 +46,7 @@ public class BaseTest {
         return driver;
     }
 
-    protected WebDriverWait getWait5() {
-        if (wait5 == null) {
-            wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-        }
-        return wait5;
+    public WebDriverWait wait(int seconds) {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(seconds));
     }
 }
