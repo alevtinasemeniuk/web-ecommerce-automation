ecf5934fae4e54fb0e5dbd6119f0ee4281b97c7a
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-03-30 21:32:42 -0700

    add RegistrationTest.java

---
+++ src/test/java/tests/RegistrationTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import io.qameta.allure.Description;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.Random;
+
+public class RegistrationTest extends BaseTest {
+
+    @Test
+    @Description("Verify that user can register with valid data")
+    public void testRegisterWithValidData() {
+
+    }
+
+}
