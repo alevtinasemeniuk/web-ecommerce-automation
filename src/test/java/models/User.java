ba5ca753622a4269752d13882993dc313c3caa79
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-03-31 17:31:51 -0700

    add classes

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -2,18 +2,28 @@ package models;
 
 public class User {
 
-    private String name;
+    private String title;
+    private String fullName;
     private String email;
     private String password;
+    private int day;
+    private Month month;
+    private int year;
+    private Address address;
 
-    public User(String name, String email, String password) {
-        this.name = name;
+    public User(String title, String fullName, String email, String password, int day, Month month, int year, Address address) {
+        this.title = title;
+        this.fullName = fullName;
         this.email = email;
         this.password = password;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.address = address;
     }
 
-    public String getName() {
-        return name;
+    public String getFullNameName() {
+        return fullName;
     }
 
     public String getEmail() {
@@ -24,9 +34,29 @@ public class User {
         return password;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public Month getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
     @Override
     public String toString(){
-        return "User Name: " + name + "\n" +
+        return "User Name: " + fullName + "\n" +
                 "Email: " + email + "\n" +
                 "Password: " + password;
     }
