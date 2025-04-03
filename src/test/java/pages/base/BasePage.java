f3c4b288c19ca210e7b2b5ef1a3dbb525ceeb853
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-04-03 13:48:12 -0700

    add logger

--- src/test/java/pages/base/BasePage.java
+++ src/test/java/pages/base/BasePage.java
@@ -3,11 +3,12 @@ package pages.base;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v85.page.Page;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
@@ -15,6 +16,7 @@ public abstract class BasePage {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
@@ -23,16 +25,19 @@ public abstract class BasePage {
     }
 
     protected void click(WebElement element) {
+        logger.info("Clicking on element: {}", element);
         wait.until(ExpectedConditions.elementToBeClickable(element)).click();
     }
 
     protected void type(WebElement element, String text) {
+        logger.info("Typing into element: {} | Text: '{}'", element, text);
         wait.until(ExpectedConditions.visibilityOf(element));
         element.clear();
         element.sendKeys(text);
     }
 
     protected String getText(WebElement element) {
+        logger.info("Getting text from element: {}", element);
         return wait.until(ExpectedConditions.visibilityOf(element)).getText();
     }
 
@@ -45,15 +50,18 @@ public abstract class BasePage {
     }
 
     protected void selectFromDropdownByValue(WebElement dropdown, String value) {
+        logger.info("Selecting value '{}' from dropdown: {}", value, dropdown);
         Select select = new Select(dropdown);
         select.selectByValue(value);
     }
 
     protected void selectFromDropdownByVisibleText(WebElement dropdown, String text) {
+        logger.info("Selecting visible text '{}' from dropdown: {}", text, dropdown);
         new Select(dropdown).selectByVisibleText(text);
     }
 
     protected void selectFromDropdownByIndex(WebElement dropdown, int index) {
+        logger.info("Selecting index '{}' from dropdown: {}", index, dropdown);
         new Select(dropdown).selectByIndex(index);
     }
 
