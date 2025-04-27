5a303ef7635d097077e723aba60da2707d29fa1b
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-04-26 21:21:55 -0700

    delete logger from BasePage.java

--- src/test/java/pages/base/BasePage.java
+++ src/test/java/pages/base/BasePage.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import pages.DeleteAccountPage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -21,7 +19,6 @@ public abstract class BasePage {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
-    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
@@ -54,28 +51,23 @@ public abstract class BasePage {
     }
 
     protected void selectFromDropdownByValue(WebElement dropdown, String value) {
-        logger.info("Selecting value '{}' from dropdown: {}", value, dropdown);
         Select select = new Select(dropdown);
         select.selectByValue(value);
     }
 
     protected void selectFromDropdownByVisibleText(WebElement dropdown, String text) {
-        logger.info("Selecting visible text '{}' from dropdown: {}", text, dropdown);
         new Select(dropdown).selectByVisibleText(text);
     }
 
     protected void selectFromDropdownByIndex(WebElement dropdown, int index) {
-        logger.info("Selecting index '{}' from dropdown: {}", index, dropdown);
         new Select(dropdown).selectByIndex(index);
     }
 
     protected void click(WebElement element) {
-        logger.info("Clicking on element: {}", element);
         wait.until(ExpectedConditions.elementToBeClickable(element)).click();
     }
 
     protected void type(WebElement element, String text) {
-        logger.info("Typing into element: {} | Text: '{}'", element, text);
         wait.until(ExpectedConditions.visibilityOf(element));
         element.clear();
         element.sendKeys(text);
@@ -101,17 +93,14 @@ public abstract class BasePage {
     }
 
     protected String getText(WebElement element) {
-        logger.info("Getting text from element: {}", element);
         return wait.until(ExpectedConditions.visibilityOf(element)).getText();
     }
 
     protected boolean isDisplayed(WebElement element) {
         try {
             boolean visible = wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
-            logger.info("Element is visible: {}", element);
             return visible;
         } catch (TimeoutException e) {
-            logger.warn("Element is NOT visible within timeout: {}", element);
             return false;
         }
     }
